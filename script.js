55a07e71694fd66842a29c6ea8b730dc3566af51
Clyde Clark <clydeclark78@gmail.com>
2020-08-08 10:20:54 -0700

    scorelist array populating correctly

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ var questionCont = document.getElementById("question-container");
 var answers = document.getElementById("answer-buttons");
 var questionDisplay = document.getElementById("question")
 var randomNum;
-var timer = 60;
+var timer;
 var timerDisplay = document.getElementById("timer");
 var scoreEl = document.getElementById("score");
-
+var scoreList = document.getElementById("score-list");
+var timerHandler;
+var initials;
+var scoreBoard = [];
+var storedScores;
 
 // Questions
 var myQuestions = [{
@@ -37,14 +41,24 @@ var myQuestions = [{
     }
 ];
 
-// When clicked, hides start button and shows questions
+// Load stored values
+function loadScores() {
+    storedScores = JSON.parse(localStorage.getItem("score-board"));
+
+    if (storedScores !== null) {
+        scoreBoard = storedScores;
+    }
+}
+
+// When clicked, hides start button, shows questions and starts the timer
 function startQuiz() {
+    loadScores();
     startBtn.classList.add("hide");
     questionCont.classList.remove("hide");
-    scoreEl.classList.remove("hide")
     populateQuestion();
     scoreEl.textContent = 0;
-    var timerHandler = setInterval(function () {
+    timer = 30;
+    timerHandler = setInterval(function () {
         timer--;
         if (timer <= 0) {
             clearInterval(timerHandler);
@@ -55,9 +69,19 @@ function startQuiz() {
 }
 
 function endQuiz() {
-    timer = 60;
-    startBtn.classList.remove("hide");
     questionCont.classList.add("hide");
+    initials = prompt("Enter your initials");
+    var user = {
+        Name: initials,
+        Score: score
+    };
+    scoreBoard.push(user);
+    if (scoreBoard.length > 5) {
+        scoreBoard.splice(0, 1);
+    }
+    localStorage.setItem("score-board", JSON.stringify(scoreBoard));
+    printScores();
+    console.log(localStorage);
 }
 
 // Randomizes display questions
@@ -78,14 +102,30 @@ function selectAnswer(event) {
     try {
         if (event.target.value === myQuestions[randomNum].correctAnswer) {
             score++;
-            scoreEl.firstChild.textContent = score;
+            scoreEl.textContent = score;
         } else {
-            timer = timer - 5;
+            timer -= 5;
         }
         myQuestions.splice(randomNum, 1);
         populateQuestion();
     } catch (error) {
+        clearInterval(timerHandler);
         endQuiz();
+    }
+}
+
+function printScores() {
+    // Clear list
+    scoreList.innerHTML = "";
+
+    // Render a new li for each item
+    for (var i = 0; i < scoreBoard.length; i++) {
+        var scoreItem = scoreBoard[i];
+
+        var li = document.createElement("li");
+        li.textContent = scoreItem;
+
+        scoreList.appendChild(li);
     }
 }
 
